cd1caff13087347f82a87bfd88c0a6a00e5fe632
Andrey Zaytsev <zaytsev2@illinois.edu>
2014-10-21 21:48:17 -0500

    Should print Works on the main page now

--- cs411app/tests.py
+++ cs411app/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from django import http
 # Create your tests here.
+
+from . import views
+
+class HomeViewTest(TestCase):
+    def test_home(self):
+        requst = http.HttpRequest()
+        response = views.home(request)
+        self.assertEqual(200, response.status_code)
+        self.assertIn('Works', response.content)
